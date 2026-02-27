62a42307a6ea6fd490c2ed9dd76a499da13ff500
James <jwyork2008@gmail.com>
2026-02-27 14:17:58 -0600

    Worked on see more button

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-$('#seeMoreSkills').on('click', function() {
-    $('#skills').
+$(document).ready(function() {
+    let newSkills = `
+        <div class="row">
+            <div class="skill-item col-8 col-md-4 text-center h-100 p-3">
+                <i class="fa-brands fa-css3-alt fa-4x" aria-hidden="true"></i>
+                <h6 class="pb-3"><b>CSS</b></h6>
+                <p>Building polished maintainable page designs using flexbox grid and component based styling</p>
+            </div>
+            <div class="skill-item col-8 col-md-4 text-center h-100 p-3">
+                <i class="fa-brands fa-bootstrap fa-4x" aria-hidden="true"></i>
+                <h6 class="pb-3"><b>Bootstrap</b></h6>
+                <p>Creating responsive webpages using Bootstrap 3 4 and 5 using utility classes for rapid development</p>
+            </div>
+                            
+            <div class="skill-item col-8 col-md-4 text-center h-100 p-3">
+                <i class="fa-brands fa-git-alt fa-4x" aria-hidden="true"></i>
+                <h6 class="pb-3"><b>Git & Github</b></h6>
+                <p>Maintaining version control workflows with Git including beanching strategies pull requests and collaborative development on Github</p>                    
+            </div>
+        </div>`
+    $('#seeMoreSkills').on('click', function() {
+        $('#seeMoreSkills').remove()
+        $('#dynamicSkills').append(newSkills)
+    })
 })
-
-/*                        <div class="skill-item col-8 col-md-4 text-center h-100 p-3">
-                            <i class="fa-brands fa-css3-alt fa-4x" aria-hidden="true"></i>
-                            <h6 class="pb-3"><b>CSS</b></h6>
-                            <p>Building polished, maintainable page designs using flexbox, grid, and component based styling</p>
-                        </div>
-
-
-                        <div class="skill-item col-8 col-md-4 text-center h-100 p-3">
-                            <i class="fa-brands fa-bootstrap fa-4x" aria-hidden="true"></i>
-                            <h6 class="pb-3"><b>Bootstrap</b></h6>
-                            <p>Creating responsive webpages using Bootstrap 3, 4, and 5, leveraging utility classes for rapid development</p> 
-                        </div>
-                        
-                        <div class="skill-item col-8 col-md-4 text-center h-100 p-3">
-                            <i class="fa-brands fa-git-alt fa-4x" aria-hidden="true"></i>
-                            <h6 class="pb-3"><b>Git & Github</b></h6>
-                            <p>Maintaining version control workflows with Git, including beanching strategies, pull requests, and collaborative development on Github</p>                    
-                        </div>
-                            */
