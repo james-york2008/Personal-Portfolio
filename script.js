efd075b958f4675fd1316f08570202a4ea2f92d6
James <jwyork2008@gmail.com>
2026-02-27 17:56:31 -0600

    Removed jQuery dependency and replaced with vanilla JS

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
-    $('#toggleMoreSkills').on('click', function() {
-        $('#dynamicSkills').toggleClass('d-md-none')
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('toggleMoreSkills')
+    const skills = dynamicSkills
+    
+    btn.addEventListener('click', () => {
+        skills.classList.toggle('d-none')
+        btn.textContent = skills.classList.contains('d-none')
+            ?'See more'
+            :'See less'
     })
 })
